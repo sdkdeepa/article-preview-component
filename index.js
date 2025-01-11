d14bbc99533fc0fe61a51386f653dd92ad054a01
Deepa Subramanian <deepa94086@gmail.com>
2025-01-11 04:05:19 -0800

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+//Desktop Share Button Toggle
+const desktopShareBtn = document.getElementById("desktop-share-btn")
+desktopShareBtn.addEventListener("click", toggleDesktopShare)
+
+function toggleDesktopShare () {
+    const shareShapeContainer = document.querySelector(".share-shape-container");
+    shareShapeContainer.classList.toggle("display-none")
+
+}
+
+// Mobile Share Toggle
+
+const mobileAuthorArea = document.querySelector(".mobile-author-area")
+const mobileSocialSharingBar = document.querySelector(".mobile-social-sharing-bar")
+const mobileShareBtnContainer = document.querySelector(".mobile-share-btn-container")
+
+mobileShareBtnContainer.addEventListener("click", function() {
+
+    mobileAuthorArea.style.display = "none"
+    mobileSocialSharingBar.style.display = "flex"
+})
+
+
+const mobileSocialSharingBtn = document.querySelector(".mobile-social-sharing-btn-container")
+mobileSocialSharingBtn.addEventListener("click", function() {
+
+    mobileAuthorArea.style.display = "flex"
+    mobileSocialSharingBar.style.display = "none"
+    
+})
